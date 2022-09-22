1613673883fa8a467dd5ff56107bf1d375c207a0
Nicolay Joahsen <77834534+Nicrus@users.noreply.github.com>
2022-09-22 12:18:47 +0200

    Player hand change, fmod sounds

--- A_Gods_Draw/Assets/__Scripts/Cards/Player_Hand.cs
+++ A_Gods_Draw/Assets/__Scripts/Cards/Player_Hand.cs
@@ -3,104 +3,83 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_Hand : MonoBehaviour
-{   public GameObject cardPrefab;
-    public Card_SO cardSo;
-    private float xVelocity,yVelocity,zVelocity;
-    public float smoothTime;
-    private Vector3 startPos;
-    private GameObject camera;
-   // private Vector3 Target;
-    //public float time;
-    // public float angle;
-     private bool holdingOver;
+{
+    private float cardRotation = 10; 
+    public List<CardInHand> CAH = new List<CardInHand>();
 
-    
-    public class CardInHand 
+    public GameObject spawnCard;
+    public class CardInHand
     {
-        public Vector3 position;
         public Card_Selector CS;
-        public Vector3 hoverPos;
+        public Animator cardAnimation;
 
-        public CardInHand()
+        public  CardInHand(Card_Selector selector)
         {
-            hoverPos = new Vector3(position.x, position.y + 0.5f, position.z -0.5f);
+            this.cardAnimation = selector.GetComponentInChildren<Animator>();
+            this.CS = selector;
+            
         }
+
+    }
+    public void AddCard(CardInHand card)
+    {  
+        
+        CAH.Add(card);  
+        
     
+        UpdateCards();
     }
+    
 
-
-    private List<CardInHand> CardsInHand = new List<CardInHand>(); 
-   
-    // Start is called before the first frame update
-    void Start()
+    public void RemoveCard(int pos)
     {
-        camera = Camera.main.gameObject;
-
-        //startPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + cardPosRelativeTohand, transform.position.z);
-        //Target = new Vector3(startPos.x, startPos.y + 20f, startPos.z - 20f);
-       // Target = new Vector3(startPosition.x, startPosition.y + 0.03f, startPosition.z - 0.15f);
-
+         
+        CAH.RemoveAt(pos);
+        UpdateCards();     
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-        if(Input.GetKeyDown(KeyCode.A))
-        {
-            GameObject newCards = Instantiate(cardPrefab);
-            newCards.GetComponent<Card_Selector>();
-            AddCard(newCards.GetComponent<Card_Selector>(), cardSo);
-            
+     private void Start()
+    {   
 
-        }
-        for (int i = 0; i < CardsInHand.Count ; i++)
+
+    }
+
+
+    private void Update()
+    {
+        for (int i = 0; i < CAH.Count; i++)
         {
+            if(CAH[i].CS.holdingOver)
             
-            if(CardsInHand[i].CS.holdingOver)
-            {   
-                Debug.Log(CardsInHand[i].CS.transform.position);
-                float newX = Mathf.SmoothDamp(transform.position.x, CardsInHand[i].hoverPos.x, ref xVelocity, smoothTime);
-                float newY = Mathf.SmoothDamp(transform.position.y, CardsInHand[i].hoverPos.y, ref yVelocity, smoothTime);
-                float newZ = Mathf.SmoothDamp(transform.position.z, CardsInHand[i].hoverPos.z, ref zVelocity, smoothTime);
-            }
-            else 
             {
-                 float oldPos = Mathf.SmoothDamp(transform.position.y, CardsInHand[i].position.y, ref yVelocity, smoothTime);
-                 CardsInHand[i].CS.transform.position = new Vector3(transform.position.x, oldPos, transform.position.y);
+                HoverOverCard(i);
+            }
+            else
+            {
+                StopHover(i);
             }
         }
+
+    }
+    private void UpdateCards()
+    {
+        for (int i = 0; i < CAH.Count; i++)
+        {
+            CAH[i].CS.transform.rotation = Quaternion.Euler(cardRotation * i,0,0);
+        }
+    }
+    
+    void HoverOverCard(int card)
+    {
+        Debug.Log("HoveringOver");
+        CAH[card].CS.transform.rotation = Quaternion.Euler(0,0,0);
+        CAH[card].cardAnimation.SetBool("ShowCard",true);
+    }
+    void StopHover(int card)
+    {
         
+        CAH[card].CS.transform.rotation = Quaternion.Euler(cardRotation * card, 0, 0);
+        CAH[card].cardAnimation.SetBool("ShowCard", false);
     }
-
-
-    public void AddCard(Card_Selector target, Card_SO card_So)
-    {
-        GameObject newPivotPoint = new GameObject("CardPivot");
-        newPivotPoint.transform.SetParent(transform, false);
-        newPivotPoint.transform.position = new Vector3(0,0f,0);
-        target.transform.SetParent(newPivotPoint.transform);
-        // listOfCards.Add(instance);
-
-        CardInHand card = new CardInHand();
-        card.CS = target;
-        card.position = newPivotPoint.transform.position + new Vector3 (0,0,0);
-        CardsInHand.Add(card);
-
-
-
-        //target = newPivotPoint.transform.position + new Vector3(2,0f,0);
-
-    }
-
-      public void RemoveCard(GameObject target, Card_SO card)
-    {
-        target.transform.parent = null;
-
-
-
-    }
-
-
 }
--- A_Gods_Draw/Assets/__Scripts/Editor/Hand_editor.cs
+++ A_Gods_Draw/Assets/__Scripts/Editor/Hand_editor.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-[CustomEditor(typeof(Player_Hand2))]
+[CustomEditor(typeof(Player_Hand))]
 public class Hand_editor : Editor 
 {
-    private Player_Hand2 script;
+    private Player_Hand script;
 
     private void OnEnable()
     {
-        script = target as Player_Hand2;
+        script = target as Player_Hand;
     }
 
 
@@ -23,7 +23,7 @@ public class Hand_editor : Editor
         {
             GameObject obj = Instantiate(script.spawnCard, new Vector3(1.47000003f,-0.140000001f,-0.370000005f), Quaternion.identity);
             
-            Player_Hand2.CardInHand card = new Player_Hand2.CardInHand(obj.GetComponentInChildren<Card_Selector>());
+            Player_Hand.CardInHand card = new Player_Hand.CardInHand(obj.GetComponentInChildren<Card_Selector>());
             script.AddCard(card);
         }    
     }
