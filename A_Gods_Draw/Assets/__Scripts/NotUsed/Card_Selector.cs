d40158c5dd87e5b89c39ef7dc781313eea6ab9f4
Nicolay Joahsen <77834534+Nicrus@users.noreply.github.com>
2022-09-23 14:37:54 +0200

    Fmod again

--- A_Gods_Draw/Assets/__Scripts/NotUsed/Card_Selector.cs
+++ A_Gods_Draw/Assets/__Scripts/NotUsed/Card_Selector.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using FMODUnity;
+//using FMODUnity;
 
 public class Card_Selector : MonoBehaviour
 { 
