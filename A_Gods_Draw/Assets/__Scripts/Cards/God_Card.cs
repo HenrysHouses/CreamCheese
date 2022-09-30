4e5bc3836dc3136805b51ae9e16ddf60b97c12e5
Javier V <javilleg@ucm.es>
2022-09-30 10:03:59 +0200

    abstract

--- A_Gods_Draw/Assets/__Scripts/Cards/God_Card.cs
+++ A_Gods_Draw/Assets/__Scripts/Cards/God_Card.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 [CreateAssetMenu(menuName = "Card/God card")]
 
-public class God_Card : Card_SO
+public abstract class God_Card : Card_SO
 {
     public short health;
 
-    public IGodActions god;
     public override Card_Behaviour Init(GameObject a)
     {
         God_Behaviour behaviour = a.AddComponent<God_Behaviour>();
