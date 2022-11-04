649e595d40b581199605141f5c3abb2b7b4b1595
Javier V <javilleg@ucm.es>
2022-11-04 19:48:59 +0100

    Update SaveJson.cs
    Had to comment out the SaveJson that is not used

--- A_Gods_Draw/Assets/__Scripts/NotUsed/SaveJson.cs
+++ A_Gods_Draw/Assets/__Scripts/NotUsed/SaveJson.cs
@@ -1,56 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.IO;
+//using UnityEngine;
 
-public static class GameSaver
-{
+//public static class GameSaver
+//{
 
 
-    private static string path = "";
-    private static string persistantPath = "";
+//    private static string path = "";
+//    private static string persistantPath = "";
 
-    public static void InitializeSaving()
-    {
-        SetPaths();
-        Debug.Log("AAAA setpaths plz!");
-    }
+//    public static void InitializeSaving()
+//    {
+//        SetPaths();
+//        Debug.Log("AAAA setpaths plz!");
+//    }
 
-    private static void SetPaths()
-    {
-        path = Application.dataPath + Path.AltDirectorySeparatorChar + "SaveData.json";
-        persistantPath = Application.persistentDataPath + Path.AltDirectorySeparatorChar + "SaveData.json";
+//    private static void SetPaths()
+//    {
+//        path = Application.dataPath + Path.AltDirectorySeparatorChar + "SaveData.json";
+//        persistantPath = Application.persistentDataPath + Path.AltDirectorySeparatorChar + "SaveData.json";
 
-        if (!Directory.Exists(persistantPath))
-        {
-            string savePath = persistantPath;
-            using StreamWriter writer = new StreamWriter(savePath);
+//        if (!Directory.Exists(persistantPath))
+//        {
+//            string savePath = persistantPath;
+//            using StreamWriter writer = new StreamWriter(savePath);
 
-            Debug.Log("file created?");
-        }
-    }
+//            Debug.Log("file created?");
+//        }
+//    }
 
-    public static void SaveData(DeckList_SO deck)
-    {
-        string savePath = persistantPath; // <- path for us to see in assets, persistantPath is what should be used
-        string json = JsonUtility.ToJson(deck);
+//    public static void SaveData(DeckList_SO deck)
+//    {
+//        string savePath = persistantPath; // <- path for us to see in assets, persistantPath is what should be used
+//        string json = JsonUtility.ToJson(deck);
 
-        using StreamWriter writer = new StreamWriter(savePath);
-        writer.Write(json);
-    }
+//        using StreamWriter writer = new StreamWriter(savePath);
+//        writer.Write(json);
+//    }
 
-    public static DeckList_SO LoadData()
-    {
-        using StreamReader streamReader = new StreamReader(persistantPath); // <- path for us to see in assets, persistantPath is what should be used
-        string json = streamReader.ReadToEnd();
+//    public static DeckList_SO LoadData()
+//    {
+//        using StreamReader streamReader = new StreamReader(persistantPath); // <- path for us to see in assets, persistantPath is what should be used
+//        string json = streamReader.ReadToEnd();
 
-        DeckList_SO deck = JsonUtility.FromJson<DeckList_SO>(json);
+//        DeckList_SO deck = JsonUtility.FromJson<DeckList_SO>(json);
 
-        Debug.Log(deck);
+//        Debug.Log(deck);
 
-        if(deck == null)
-            deck = DeckManager_SO.getStarterDeck();
+//        if(deck == null)
+//            deck = DeckManager_SO.getStarterDeck();
 
-        return deck;
-    }
-}
+//        return deck;
+//    }
+//}
