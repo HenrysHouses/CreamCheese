b6285bc23904a48292cd77330f8dd92caed0d8aa
Nicolay Joahsen <77834534+Nicrus@users.noreply.github.com>
2022-11-04 11:55:44 +0100

    HiwToPlay panel and some cards

---
+++ A_Gods_Draw/Assets/__Scripts/Menu/MenuButtons.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtons : MonoBehaviour
+{
+    public GameObject HTPPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HTPPanel.SetActive(false);
+    }
+
+    public void ShowHowToPlay()
+    {
+        HTPPanel.SetActive(true);
+    }
+
+    public void HideHowToPlay()
+    {
+        HTPPanel.SetActive(false);
+    }
+}
