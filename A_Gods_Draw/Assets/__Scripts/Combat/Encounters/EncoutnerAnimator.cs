21e21fd6778989945255cdc01110d0a193a17c45
roock89 <77836346+HenrysHouses@users.noreply.github.com>
2022-12-02 16:55:41 +0100

    encounter start particles

--- A_Gods_Draw/Assets/__Scripts/Combat/Encounters/EncoutnerAnimator.cs
+++ A_Gods_Draw/Assets/__Scripts/Combat/Encounters/EncoutnerAnimator.cs
@@ -3,6 +3,7 @@ using HH.MultiSceneTools.Examples;
 
 public class EncoutnerAnimator : MonoBehaviour
 {
+    [SerializeField] GameObject Particles;
     [SerializeField] Transform[] AnimateTransforms;
     [SerializeField] SceneTransition BoardTransition;
     [SerializeField] float speed = 1;
@@ -24,7 +25,11 @@ public class EncoutnerAnimator : MonoBehaviour
     {
         if(!BoardTransition.isTransitioning)
         {
+            if(Particles)
+                Particles.SetActive(true);
             AnimateTransforms[1].gameObject.SetActive(true);
+            Destroy(Particles, Particles.GetComponent<ParticleSystem>().main.duration+3);
+            Light light = Particles.GetComponentInChildren<Light>();
 
             for (int i = 0; i < AnimateTransforms.Length; i++)
             {
@@ -38,8 +43,12 @@ public class EncoutnerAnimator : MonoBehaviour
 
                 totalOffset += offset;
 
+                light.intensity -= Time.deltaTime * 1.5f;
+
                 if(totalOffset >= startOffset)
+                {
                     Destroy(this);
+                }
             }
         }
     }
