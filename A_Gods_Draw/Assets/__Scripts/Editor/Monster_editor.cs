d621a73f619fd24348fd60ea98596e8c448b90eb
kevin hagfors <kevin.hagfors@gmail.com>
2023-04-21 13:53:04 +0200

    editor done

--- A_Gods_Draw/Assets/__Scripts/Editor/Monster_editor.cs
+++ A_Gods_Draw/Assets/__Scripts/Editor/Monster_editor.cs
@@ -10,14 +10,16 @@ public class Monster_Editor : Editor
 
     SerializedProperty EnemyActions;
     private EnemyIntent actionSelected;
-    private bool actionsDropdown, useStrengthMod;
+    private bool actionListDropdown, useStrengthMod;
     private float sliderVal;
+    private List<bool> actionDropdowns;
 
     private void OnEnable()
     {
 
         EnemyActions = serializedObject.FindProperty("EnemyActions");
-        actionsDropdown = false;
+        actionDropdowns = new List<bool>(EnemyActions.arraySize);
+        actionListDropdown = false;
 
     }
 
@@ -27,14 +29,25 @@ public class Monster_Editor : Editor
         serializedObject.Update();
         
         EditorGUILayout.LabelField("Enemy Actions Setup");
-        actionsDropdown = EditorGUILayout.Foldout(actionsDropdown, "Actions", true);
-        if(actionsDropdown)
+        actionListDropdown = EditorGUILayout.Foldout(actionListDropdown, "Actions", true, EditorStyles.foldoutHeader);
+        if(actionListDropdown)
         {
 
             EditorGUI.indentLevel++;
 
             for(int i = 0; i < EnemyActions.arraySize; i++)
             {
+
+                EditorGUILayout.Space();
+
+                if((actionDropdowns.Count - 1) < i)
+                    actionDropdowns.Add(false);
+                actionDropdowns[i] = EditorGUILayout.Foldout(actionDropdowns[i], "Action " + i, true, EditorStyles.foldoutHeader);
+
+                if(!actionDropdowns[i])
+                    continue;
+
+                EditorGUI.indentLevel++;
 
                 SerializedProperty _actionSelection = EnemyActions.GetArrayElementAtIndex(i);
 
@@ -52,12 +65,12 @@ public class Monster_Editor : Editor
                 else
                     _actionSelection.Next(false);//AllRequired
 
-                //EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.BeginHorizontal();
                 _actionSelection.Next(false);//MinStrength
                 EditorGUILayout.PropertyField(_actionSelection);
                 _actionSelection.Next(false);//MaxStrength
                 EditorGUILayout.PropertyField(_actionSelection);
-                //EditorGUILayout.EndHorizontal();
+                EditorGUILayout.EndHorizontal();
 
                 useStrengthMod = EditorGUILayout.Toggle("Use Strength modifications", useStrengthMod);
                 _actionSelection.Next(false);//UseStrengthMod
@@ -104,7 +117,30 @@ public class Monster_Editor : Editor
                 _actionSelection.Next(false);//SFX
                 EditorGUILayout.PropertyField(_actionSelection);
 
-                serializedObject.ApplyModifiedProperties();
+                EditorGUILayout.BeginHorizontal();
+
+                if(GUILayout.Button("+", EditorStyles.miniButtonLeft))
+                {
+
+                    EnemyActions.InsertArrayElementAtIndex(EnemyActions.arraySize);
+                    actionDropdowns.Add(true);
+
+                }
+
+                if(GUILayout.Button("-", EditorStyles.miniButtonRight))
+                {
+    
+                    int _arraySize = EnemyActions.arraySize;
+                    EnemyActions.DeleteArrayElementAtIndex(i);
+                    actionDropdowns.RemoveAt(i);
+                    if(EnemyActions.arraySize == _arraySize)
+                        EnemyActions.DeleteArrayElementAtIndex(i);
+
+                }
+
+                EditorGUILayout.EndHorizontal();
+
+                EditorGUI.indentLevel--;
 
             }
 
@@ -112,6 +148,7 @@ public class Monster_Editor : Editor
 
         }
 
+        serializedObject.ApplyModifiedProperties();
         base.OnInspectorGUI();
 
     }
