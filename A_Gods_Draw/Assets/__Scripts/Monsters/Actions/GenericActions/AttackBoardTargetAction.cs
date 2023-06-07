fad257dd72390f018a681c784bf8b53918f0d839
WizyWoo <72544537+WizyWoo@users.noreply.github.com>
2023-06-07 16:38:37 +0200

    auto Commit

--- A_Gods_Draw/Assets/__Scripts/Monsters/Actions/GenericActions/AttackBoardTargetAction.cs
+++ A_Gods_Draw/Assets/__Scripts/Monsters/Actions/GenericActions/AttackBoardTargetAction.cs
@@ -11,19 +11,51 @@ public class AttackBoardTargetAction : MonsterAction
     {
 
         ActionID = (int)EnemyIntent.AttackPlayer;
-        actionIcon = Resources.Load<Sprite>("EnemyData/Icons/Icon_Buff_IMG_v7");
-        desc = "This enemy will attack a target on the board";
+        actionIcon = Resources.Load<Sprite>("EnemyData/Icons/Icon_AttackingChain_IMG_v1");
+        desc = "Fenrir will attack the chains on the board";
         ActionIntentType = IntentType.Attack;
+
+    }
+
+    public override void SelectTargets(BoardStateController _board)
+    {
+
+        ResetTargets();
+
+        List<BoardTarget> _targets = _board.ActiveExtraEnemyTargets;
+
+        if(_targets.Count == 0)
+        {
+
+            Self.CancelIntent();
+            return;
+
+        }
+
+        for(int i = 0; i < _targets.Count; i++)
+        {
+
+            ITargets.Add(_targets[Random.Range(0, _targets.Count)]);
+            _targets[i].Targeted(Self.gameObject);
+
+        }
 
     }
 
     public override void PerformAction(BoardStateController _board, int _strength, object _source)
     {
-        
-        if(_board.ActiveExtraEnemyTargets.Count <= 0)
-            return;
 
-        _board.ActiveExtraEnemyTargets[Random.Range(0, _board.ActiveExtraEnemyTargets.Count)].DealDamage(_strength);
+        for(int i = 0; i < ITargets.Count; i++)
+        {
+
+            if(ITargets[i] == null)
+                continue;
+            
+            ITargets[i].DealDamage(_strength);
+
+        }
+
+        ResetTargets();
 
         Monster _enemy = _source as Monster;
         if(_enemy)
@@ -31,7 +63,7 @@ public class AttackBoardTargetAction : MonsterAction
             _enemy.Animator.SetTrigger("Attack");
             _enemy.PlaySound(ActionSFX);
         }
-        
+
     }
 
 }
